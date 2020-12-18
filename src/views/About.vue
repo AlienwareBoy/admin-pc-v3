75011eb71168e0a660a53218c1fb853c128d33a0
alienware <13922289159@163.com>
2020-12-18 18:42:06 +0800

    feat:update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div>{{name}}</div>
 </template>
+
+<script lang='ts'>
+import { reactive, toRefs, onBeforeMount, onMounted, onUnmounted } from "vue";
+let timer: number = 1;
+interface DataProps {
+  name: number;
+}
+export default {
+  name: "sdf",
+  setup() {
+    const data: DataProps = reactive({
+      name: 123,
+    });
+    onBeforeMount(() => {
+      console.log("2.组件挂载页面之前执行----onBeforeMount");
+    });
+    onMounted(() => {
+      console.log("3.-组件挂载到页面之后执行-------onMounted");
+    });
+    onUnmounted(() => {
+      console.log(new Date().getTime() - timer);
+    });
+    const refData = toRefs(data);
+    return {
+      ...refData,
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
